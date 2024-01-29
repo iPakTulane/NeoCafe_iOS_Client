c5e79b82e2f954db787124b327f02a78b6d185fd
Igor Pak <ipak.tulane@gmail.com>
2024-01-29 22:43:22 +0500

    Initial Commit

---
+++ NeoCafe_iOS_Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeoCafe_iOS_Client
+//
+//  Created by iPak Tulane on 29/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
